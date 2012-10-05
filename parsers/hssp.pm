6d32a316ac11da75a355285e2be8ec1dd24f770a
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2012-10-05 08:57:30 +0000

    better hssp parser

--- parsers/hssp.pm
+++ parsers/hssp.pm
@@ -13,26 +13,55 @@ sub parse
 {
 	my ($self, $text) = @_;
 
-	while ($text =~ m/^.+$/mg)
-	{
-		my $line = $&;
+	open(my $h, "<", \$text);
 
-		last if ($line =~ /^## ALIGNMENTS/);
+	while (my $line = <$h>)
+	{
+		last if (substr($line, 0, 11) eq '## PROTEINS');
 		
-		if ($line =~ /^PDBID\s+(\S+)/)
+		my ($fld, $value) = ($1, $2) if $line =~ m/^(\S+?)\s+(.*)/;
+		
+		next unless defined $fld;
+		next if $fld eq 'NOTATION';
+		
+		if ($fld eq 'PDBID')
 		{
-			$self->index_unique_string('id', $1);
-			$self->set_attribute('id', $1);
+			$self->index_unique_string('id', $value);
+			$self->set_attribute('id', $value);
 		}
-		elsif ($line =~ /^(\S+?)\s+(.*)/)
+		elsif ($fld eq 'DBREF')
 		{
-			my $fld = $1;
-			$line = $2;
+			#DBREF      1CR7 A    1   236  PIR    S14765   S14765           1    236
+			my $db = substr($line, 30,  7);	$db =~ s/\s+$//;
+			my $ac = substr($line, 37,  9);	$ac =~ s/\s+$//; 
+			my $id = substr($line, 46, 12);	$id =~ s/\s+$//;
 			
-			$line =~ s/(\w)\.(?=\w)/$1. /og
+			my %dbmap = (
+				embl	=> 'embl',
+				gb		=> 'genbank',
+				ndb		=> 'ndb',
+				pdb		=> 'pdb',
+				pir		=> 'pir',
+				prf		=> 'profile',
+				sws		=> 'uniprot',
+				trembl	=> 'uniprot',
+				unp		=> 'uniprot'
+			);
+
+			$db = $dbmap{lc($db)} if defined $dbmap{lc($db)};
+			
+			if (length($db) > 0)
+			{
+				$self->add_link($db, $id) if length($id);
+				$self->add_link($db, $ac) if length($ac);
+			}
+		}
+		else
+		{
+			$value =~ s/(\w)\.(?=\w)/$1. /og
 				if ($fld eq 'AUTHOR');
 
-			$self->set_attribute('title', lc($line))
+			$self->set_attribute('title', lc($value))
 				if ($fld eq 'HEADER');
 
 			my %fldmap = (
@@ -43,13 +72,27 @@ sub parse
 			);
 
 			if (defined $fldmap{$fld}) {
-				my $value = $1 if ($line =~ m/^(\d+)/);
+				my $value = $1 if ($value =~ m/^(\d+)/);
 				$self->index_number(lc $fld, $value);
 			}
 			else {
-				$self->index_text('text', $line);
+				$self->index_text('text', $value);
 			}
 		}
+	}
+
+	<$h>;
+	while (my $line = <$h>)
+	{
+		last if (substr($line, 0, 13) eq '## ALIGNMENTS');
+		
+		my $id = substr($line, 8, 12); $id =~ s/\s+$//;
+		my $strid = substr($line, 20, 4);
+		my $acc = substr($line, 80, 10); $acc =~ s/\s+$//;
+		
+		$self->index_string('seqid', $id);
+		$self->index_string('strid', $id) unless $strid eq '    ';
+		$self->index_string('seqacc', $id);
 	}
 }
 

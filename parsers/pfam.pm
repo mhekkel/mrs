ea0b21df6b5f4678dc28a8bbaff7a5a30ae6b43a
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2012-10-10 10:00:16 +0000

    pfam parser

--- parsers/pfam.pm
+++ parsers/pfam.pm
@@ -1,69 +1,6 @@
-# MRS plugin for creating an pfam db
-#
-# $Id: pfam.pm 18 2006-03-01 15:31:09Z hekkel $
-#
-# Copyright (c) 2005
-#      CMBI, Radboud University Nijmegen. All rights reserved.
-#
-# This code is derived from software contributed by Maarten L. Hekkelman
-# and Hekkelman Programmatuur b.v.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#        This product includes software developed by the Radboud University
-#        Nijmegen and its contributors.
-# 4. Neither the name of Radboud University nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE RADBOUD UNIVERSITY AND CONTRIBUTORS
-# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# 2007-02-23 some changes by Guy Bottu from BEN
-#   long names for fields
-#   nseq field
-#   version subroutine
+package M6::Script::pfam;
 
-package MRS::Script::pfam;
-
-our @ISA = "MRS::Script";
-
-my %INDICES = (
-	'id' => 'Identification',
-	'ac' => 'Accession number',
-	'au' => 'Entry author',
-	'am' => 'Alignment method (localfirst, globalfirst or byscore)',
-	'cc' => 'Comments and Notes',
-	'dc' => 'Comment about database reference',
-	'de' => 'Description',
-	'dr' => 'Database cross-reference',
-	'gs' => 'UniProt sequence cross-reference',
-	'ne' => 'Pfam accession number nested domain',
-	'nseq' => 'The number of sequences per cluster',
-	'nl' => 'Location nested domain',
-	'pi' => 'Previous identifier',
-	'ref' => 'Any reference field',
-	'se' => 'Source of seed',
-	'sq' => 'Number of sequences in alignment',
-	'tp' => 'Type (Family, Domain, Motif or Repeat)',
-);
+our @ISA = "M6::Script";
 
 my @links = (
 	{
@@ -107,107 +44,69 @@ sub new
 {
 	my $invocant = shift;
 	
-	my $self = new MRS::Script(
-		url		=> 'http://pfam.sanger.ac.uk/',
-		section	=> 'other',
-		meta	=> [ 'title' ],
-		indices	=> \%INDICES,
+	my $self = new M6::Script(
+		firstdocline => '# STOCKHOLM 1.0',
+		lastdocline => '//',
 		@_
 	);
 	
-	if (defined $self->{db}) {
-		
-		my %NAMES = (
-			pfama		=> 'Pfam-A',
-			pfamb		=> 'Pfam-B',
-			pfamseed	=> 'Pfam-Seed'
-		);
-
-		$self->{name} = $NAMES{$self->{db}};
-		
-		my %FILES = (
-			pfama		=> qr/Pfam-A\.full\.gz/,
-			pfamb		=> qr/Pfam-B\.gz/,
-			pfamseed	=> qr/Pfam-A\.seed\.gz/,
-		);
-		
-		$self->{raw_dir} =~ s|pfam[^/]+/?$|pfam|;
-		$self->{raw_files} = $FILES{$self->{db}};
-	}
-	
-	return bless $self, "MRS::Script::pfam";
+	return bless $self, "M6::Script::pfam";
 }
 
-sub Parse
+sub parse
 {
-	my $self = shift;
+	my ($self, $text) = @_;
 	
-	my ($doc, $state, $m);
-	
-	$state = 0;
-	
-	my $lookahead = $self->GetLine;
-	while (defined $lookahead)
+	open(my $h, "<", \$text);
+	while (my $line = <$h>)
 	{
-		my $line = $lookahead;
-		$lookahead = $self->GetLine;
-
-		$doc .= $line;
-
 		chomp($line);
 
-		if ($line eq '//')
-		{
-			$self->Store($doc);
-			$self->FlushDocument;
-
-			$doc = undef;
-		}
-		elsif (substr($line, 0, 5) eq '#=GF ')
+		if (substr($line, 0, 5) eq '#=GF ')
 		{
 			my $field = substr($line, 5, 2);
 			my $value = substr($line, 10);
 
 			if ($field eq 'ID')
 			{
-				$self->IndexValue('id', $value);
+				$self->index_unique_string('id', $value);
 			}
 			elsif ($field =~ /BM|GA|TC|NC/o)  # useless fields
 			{}
 			elsif ($field eq 'AC' and $value =~ m/(P[BF]\d+)/)
 			{
 				$value = $1;
-				$self->IndexValue('ac', $value);
+				$self->index_string('ac', $value);
 			}
 			elsif ($field eq 'DR')
 			{
 				my @link = split(m/; */, $value);
-				$self->IndexLink($link[0], $link[1]) if length($link[0]) > 0 and length($link[1]) > 0;
+				$self->add_link($link[0], $link[1]) if length($link[0]) > 0 and length($link[1]) > 0;
 				
-				$self->IndexText('ref', $value);
+				$self->index_text('ref', $value);
 			}
 			elsif (substr($field, 0, 1) eq 'R')
 			{
-				$self->IndexText('ref', $value);
+				$self->index_text('ref', $value);
 			}
 			elsif (substr($field, 0, 2) eq 'SQ')
 			{
-				$self->IndexNumber('nseq', $value);
+				$self->index_number('nseq', $value);
 			}
 			else
 			{
-				$self->StoreMetaData('title', $value) if $field eq 'DE';
-				$self->IndexText(lc($field), $value);
+				$self->set_attribute('title', $value) if $field eq 'DE';
+				$self->index_text(lc($field), $value);
 			}
 		}
 		elsif (substr($line, 0, 5) eq '#=GS ')
 		{
 			#		#=GS Q9ZNY5_SECCE/28-72   
 			my $link = substr($line, 26);
-			$self->IndexLink('uniprot', $1) if ($line =~ m/^AC (.+?)(\.\d)/);
-			$self->IndexLink('pdb', $1) if ($line =~ m/^DR PDB; (\w{4})/);
+			$self->add_link('uniprot', $1) if ($line =~ m/^AC (.+?)(\.\d)/);
+			$self->add_link('pdb', $1) if ($line =~ m/^DR PDB; (\w{4})/);
 			
-			$self->IndexText('gs', substr($line, 5));
+			$self->index_text('gs', substr($line, 5));
 		}
 	}
 }

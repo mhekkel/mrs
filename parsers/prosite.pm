0aac7b9c56eddee52102be610f259d25601d0c22
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2012-10-10 11:21:05 +0000

    prosite

---
+++ parsers/prosite.pm
@@ -0,0 +1,80 @@
+package M6::Script::prosite;
+
+use strict;
+use warnings;
+
+our @ISA = "M6::Script";
+
+sub new
+{
+	my $invocant = shift;
+	my $self = new M6::Script(
+		headerline => qr/^CC.+/,
+		lastdocline => '//',
+		@_
+	);
+	return bless $self, "M6::Script::prosite";
+}
+
+sub parse
+{
+	my ($self, $text) = @_;
+	
+	open(my $h, "<", \$text);
+	while (my $line = <$h>)
+	{
+		chomp($line);
+
+		if ($line =~ /^([A-Z]{2}) {3}/o)
+		{
+			my $fld = $1;
+			my $value = substr($line, 5);
+
+			if ($fld eq 'ID' and $value =~ /^([A-Z0-9_]+); ([A-Z]+)/o)
+			{
+				die "Double ID: $id <=> $1\n" if defined $id;
+				$id = $1;
+				die "ID too short" unless length($id);
+				$self->set_attribute('id', $id);
+				$self->index_unique_string('id', $id);
+				$self->index_string('type', lc $2);
+			}
+			elsif ($fld =~ /MA|NR|PA/o)  # useless fields
+			{}
+			elsif ($fld eq 'DR')
+			{
+				my @l = split(m/;\s*/, $value);
+				foreach my $l (@l)
+				{
+					$self->add_link('uniprot', $1)
+						if ($l =~ m/^(\w+),/);
+				}
+				
+				$self->index_text(lc $fld, $value);
+			}
+			elsif ($fld eq '3D')
+			{
+				my @l = split(m/;\s*/, $value);
+				foreach my $l (@l)
+				{
+					$self->add_link('pdb', $l);
+				}
+				
+				$self->index_text(lc $fld, $value);
+			}
+			elsif ($fld eq 'DO')
+			{
+				$self->add_link('prosite_doc', $1)
+					if ($value =~ m/(PDOC\d+)/);
+				$self->index_text(lc $fld, $value);
+			}
+			else
+			{
+				$self->set_attribute('title', $value) if $fld eq 'DE';
+				$self->index_text(lc($fld), $value);
+			}
+		}
+	}
+}
+
+1;

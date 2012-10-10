a143a0344e947fce75a33eeb04a6c7c6fb5bd120
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2012-10-10 12:39:44 +0000

    prosite-doc, rebase, unigene and uniref

---
+++ parsers/prosite_doc.pm
@@ -0,0 +1,44 @@
+package M6::Script::prosite_doc;
+
+our @ISA = "M6::Script";
+
+sub new
+{
+	my $invocant = shift;
+	my $self = new M6::Script(
+		lastdocline => '{END}',
+		@_
+	);
+	
+	return bless $self, "M6::Script::prosite_doc";
+}
+
+sub parse
+{
+	my ($self, $text) = @_;
+	
+	open(my $h, "<", \$text);
+	my $title;
+	
+	while (my $line = <$h>)
+	{
+		chomp($line);
+
+		if ($line =~ /^\{(PDOC\d+)\}/o)
+		{
+			$self->set_attribute('id', $1);
+			$self->index_unique_string('id', $1);
+		}
+		elsif (not defined $title and $line =~ m/\*\s*([^*].+?)\s*\*/) {
+			$self->set_attribute('title', $1);
+			$self->index_text('text', $1);
+			$title = $1;
+		}
+		else
+		{
+			$self->IndexText('text', $line);
+		}
+	}
+}
+
+1;
---
+++ parsers/unigene.pm
@@ -0,0 +1,47 @@
+package M6::Script::unigene;
+
+use strict;
+use warnings;
+
+our @ISA = "M6::Script";
+
+sub new
+{
+	my $invocant = shift;
+	my $self = new M6::Script(
+		lastdocline => '//',
+		@_
+	);
+	return bless $self, "M6::Script::unigene";
+}
+
+sub parse
+{
+	my ($self, $text) = @_;
+	
+	open(my $h, "<", \$text);
+	while (my $line = <$h>)
+	{
+		chomp($line);
+
+		if ($line =~ /^(\S+)\s+(.+?)\s*$/o)
+		{
+			my $fld = $1;
+			my $value = $2;
+
+			if ($fld eq 'ID' and $value =~ /(^\S+)/)
+			{
+				$id = $1;
+				$self->index_unique_string('id', $id);
+				$self->set_attribute('id', $id);
+			}
+			else
+			{
+				$self->set_attribute('title', $value) if $fld eq 'TITLE';
+				$self->index_text(lc($fld), $value);
+			}
+		}
+	}
+}
+
+1;

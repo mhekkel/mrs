5eec0ca6beeb0adadd01c5504457c50968c89e25
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2013-01-09 15:06:53 +0000

    mobile

---
+++ docroot/scripts/mrs-m.js
@@ -0,0 +1,22 @@
+﻿/* JavaScript for mobile version */
+
+var M6M = {
+	
+	init: function(hitCount) {
+		var results = $("#tabel");
+		
+		if (results != null && hitCount > 0)
+		{
+			$(window).scroll(function() {
+				var pageHeight = document.documentElement.scrollHeight;
+				var clientHeight = document.documentElement.clientHeight;
+				if ((pageHeight - ($(window).scrollTop() + clientHeight)) < 50)
+					M6M.extend(results, hitCount);
+			});
+		}
+	},
+	
+	extend: function(results, hitCount) {
+		alert(hitCount);
+	}
+};

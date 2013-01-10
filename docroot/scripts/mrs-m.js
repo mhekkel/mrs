e91330f40193f9aaf3d8750b3c6f5b54445c0cd2
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2013-01-10 06:33:31 +0000

    mobile support

--- docroot/scripts/mrs-m.js
+++ docroot/scripts/mrs-m.js
@@ -1,22 +1,67 @@
 ﻿/* JavaScript for mobile version */
 
 var M6M = {
+	callActive: false,
 	
-	init: function(hitCount) {
+	init: function(hitCount, db) {
 		var results = $("#tabel");
 		
 		if (results != null && hitCount > 0)
 		{
 			$(window).scroll(function() {
+				if (M6M.callActive)
+					return;
+				M6M.callActive = true;
+			
 				var pageHeight = document.documentElement.scrollHeight;
 				var clientHeight = document.documentElement.clientHeight;
 				if ((pageHeight - ($(window).scrollTop() + clientHeight)) < 50)
-					M6M.extend(results, hitCount);
+					M6M.extend(results, hitCount, db);
+				else
+					M6M.callActive = false;
 			});
 		}
 	},
 	
-	extend: function(results, hitCount) {
-		alert(hitCount);
+	extend: function(results, hitCount, db) {
+		var rowCount = results.find("tr").length - 1;
+		var count = hitCount - rowCount;
+		if (count > 5)
+			count = 5;
+		if (rowCount < hitCount)
+		{
+			callActive = true;
+		
+			jQuery.post("ajax/search", {
+				db: db, q: 'rds', offset: rowCount, count: count
+			}, function(data, status, jqXHR) {
+				if (status == "success")
+				{
+					if (data.error != null && data.error.length > 0) {
+						alert(data.error);
+					}
+					
+					$(data.hits).each(function() {
+						var lastRow = results.find(" tr:last");
+						var newRow = lastRow.clone();
+						var cells = newRow.find("td");
+						
+						cells.eq(0).text(this.nr);
+						
+						var a = cells.eq(1).find("a");
+						a.text(this.id);
+						a.attr("href", "entry?db=" + db + "&id=" + this.id);
+
+						cells.eq(2).text(this.title);
+						
+						lastRow.after(newRow);
+						newRow.show();
+					});
+				}
+				M6M.callActive = false;
+			}, "json");
+		}
+		else
+			M6M.callActive = false;
 	}
 };

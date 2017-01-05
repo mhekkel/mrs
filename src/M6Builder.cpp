46682ff035f95ec7435bf081b6334c172c4a0b83
cbaakman <c.baakman@umcn.nl>
2017-01-05 10:56:49 +0100

    undo c4cfc6f4ac4af2e596c222eaba5d0fd994b1d7cb, since it's rubbish (#63)
    Commit #c4cfc6f4ac4af2e596c222eaba5d0fd994b1d7cb causes updates to keep hanging forever. I'm undoing it now.

--- src/M6Builder.cpp
+++ src/M6Builder.cpp
@@ -540,7 +540,7 @@ void M6Processor::Process(vector<fs::path>& inFiles, M6Progress& inProgress,
     else
     {
         mUseDocQueue = true;
-        for (uint32 i = inFiles.size(); i < inNrOfThreads; ++i)
+        for (uint32 i = 0; i < inNrOfThreads; ++i)
             mDocThreads.create_thread([this]() { this->ProcessDocument(); });
     }
 
@@ -556,10 +556,14 @@ void M6Processor::Process(vector<fs::path>& inFiles, M6Progress& inProgress,
     }
     else
     {
-/*
         if (inNrOfThreads > inFiles.size())
             inNrOfThreads = static_cast<uint32>(inFiles.size());
- */
+
+        for (uint32 i = 0; i < inNrOfThreads; ++i)
+            mFileThreads.create_thread(
+                [&inProgress, this]() { this->ProcessFile(inProgress); }
+            );
+
         for (fs::path& file : inFiles)
         {
             if (not (mException == std::exception_ptr()))
@@ -577,11 +581,6 @@ void M6Processor::Process(vector<fs::path>& inFiles, M6Progress& inProgress,
         mFileQueue.Put(fs::path());
 
         // Now all the input files have been added to the queue.
-
-        for (uint32 i = 0; i < inNrOfThreads; ++i)
-            mFileThreads.create_thread(
-                [&inProgress, this]() { this->ProcessFile(inProgress); }
-            );
 
         mFileThreads.join_all();
     }
